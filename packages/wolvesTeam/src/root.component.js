d15f4b2a8010bf5fe6a86a4d277944c175eecb60
Manuel F. Del Toro <mdeltoro@belatrixsf.com>
2019-07-24 10:33:50 -0500

    first commit

---
+++ packages/wolvesTeam/src/root.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class root extends Component {
+  render() {
+    return (
+      <div>
+        Hola Wolves
+      </div>
+    )
+  }
+}
+
